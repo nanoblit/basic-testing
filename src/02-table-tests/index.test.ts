f6104c2e7351ead3dc70e101db8ee8fd1c980a5e
nanoblit <jakub.k.maleta@gmail.com>
2024-10-17 18:42:31 +0200

    finish table subtask

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,34 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
-const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+const testCases: [
+  a: unknown,
+  b: unknown,
+  action: unknown,
+  expected: number | null,
+][] = [
+  [1, 2, Action.Add, 3],
+  [-1, -2, Action.Add, -3],
+  [1, -2, Action.Add, -1],
+  [1, 2, Action.Subtract, -1],
+  [-1, -2, Action.Subtract, 1],
+  [1, -2, Action.Subtract, 3],
+  [2, 2, Action.Multiply, 4],
+  [0, 2, Action.Multiply, 0],
+  [-1, -2, Action.Multiply, 2],
+  [3, 3, Action.Divide, 1],
+  [3, 0, Action.Divide, Infinity],
+  [-4, 2, Action.Divide, -2],
+  [2, 3, Action.Exponentiate, 8],
+  [3, 2, Action.Exponentiate, 9],
+  [4, 0, Action.Exponentiate, 1],
+  [2, 3, 'Hello', null],
+  [{}, 3, Action.Add, null],
+  [2, 'Test', Action.Add, null],
+];
 
-describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+describe.each(testCases)('simpleCalculator', (a, b, action, expectedOutput) => {
+  test(`should return ${expectedOutput} for a: ${a}, b: ${b}, action: ${action}`, () => {
+    expect(simpleCalculator({ a, b, action })).toBe(expectedOutput);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
